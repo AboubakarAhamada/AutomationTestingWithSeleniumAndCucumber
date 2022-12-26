027100565ce74555c7ed171c8bbdc24eb37e04a4
AboubakarAhamada <aboumarshal@hotmail.fr>
2022-12-26 17:08:31 +0100

    Add another awesome scenario for test sending message with valid email

--- src/test/java/stepDefinitions/ContactStep.java
+++ src/test/java/stepDefinitions/ContactStep.java
@@ -3,6 +3,7 @@ package stepDefinitions;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -18,6 +19,9 @@ public class ContactStep {
     private final static String url = "https://cv-aboubakarahamada.netlify.app/contact/contact.html";
     private final static By name = By.id("name");
     private final static By email = By.id("email");
+
+    private final static By subject = By.id("subject");
+    private final static By message = By.id("message");
     private final static By sendBtn = By.className("btn");
     @BeforeAll
     public static void beforeAll(){
@@ -53,4 +57,29 @@ public class ContactStep {
         Assert.assertEquals(actualUrl,url);
     }
 
+
+    @Given("user is on contact page")
+    public void userIsOnContactPage() {
+        driver.get(url);
+    }
+
+    @When("user fulled form contact with {string} as name, {string} as email, {string} as object and {string} as message")
+    public void userFulledFormContactWithAsNameAsEmailAsObjectAndAsMessage(
+            String arg_name, String arg_email, String arg_subject, String arg_message)
+    {
+        driver.findElement(name).sendKeys(arg_name);
+        driver.findElement(email).sendKeys(arg_email);
+        driver.findElement(subject).sendKeys(arg_subject);
+        driver.findElement(message).sendKeys(arg_message);
+    }
+
+    @And("user clicks send message button")
+    public void userClicksSendMessageButton(){
+        driver.findElement(sendBtn).click();
+    }
+    @Then("user is redirected to CAPTCHA page")
+    public void userIsRedirectedToCAPTCHAPage() {
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl,"https://formsubmit.co/ed1d536d07089e40e71a5279c11fe729");
+    }
 }
--- src/test/resources/features/contact.feature
+++ src/test/resources/features/contact.feature
@@ -8,3 +8,9 @@ Feature: Test Contact Form
     |ali   | ali@  |
     |karim benzema|k.benzema|
     |Toni Kroos|@kroos|
+
+  Scenario: Send message with valid email
+    Given user is on contact page
+    When user fulled form contact with "Monsieur Bonhomme" as name, "m.bonhome@gmail.com" as email, "Besoin de collaboration" as object and "Appelez moi sur le numéro 0600000000" as message
+    And user clicks send message button
+    Then user is redirected to CAPTCHA page
